6ced2186142ccd2a3b11273dd875bd69e206aab4
Vasaluk <101427439+Vasaluk@users.noreply.github.com>
2022-05-11 20:27:56 +0300

    Update CarsController.java

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,13 +1,10 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import web.config.WebConfig;
 import web.service.CarService;
 
 
